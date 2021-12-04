9718ea462efdde3035df56d01ea58426d690930c
Artem Chumak <artem.4x@gmail.com>
2021-12-05 00:17:25 +0300

    feat(main page): add portfolio block

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,13 +12,34 @@ function Footer() {
         <p className="footer__copyright">© 2020</p>
         <ul className="footer__link-box">
           <li>
-            <a className="footer__link" href="https://practicum.yandex.ru/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
+            <a
+              className="footer__link"
+              href="https://practicum.yandex.ru/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Яндекс.Практикум
+            </a>
           </li>
           <li>
-            <a className="footer__link" href="https://github.com/artem-chumak" target="_blank" rel="noreferrer">Github</a>
+            <a
+              className="footer__link"
+              href="https://github.com/artem-chumak"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Github
+            </a>
           </li>
           <li>
-            <a className="footer__link" href="https://www.facebook.com/artem.chumak.94" target="_blank" rel="noreferrer">Facebook</a>
+            <a
+              className="footer__link"
+              href="https://www.facebook.com/artem.chumak.94"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Facebook
+            </a>
           </li>
         </ul>
       </div>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,14 +8,14 @@ import Portfolio from "../Portfolio/Portfolio";
 
 function Main() {
   return (
-    <div className="main">
+    <main className="main">
       <NavTab />
       <Promo />
       <AboutProject />
       <Techs />
       <AboutMe />
       <Portfolio />
-    </div>
+    </main>
   );
 }
 
