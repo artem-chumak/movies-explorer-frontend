12f39bb0dc5e6b1331f00b70535bd494cd9f7dcc
Artem Chumak <artem.4x@gmail.com>
2021-12-05 05:34:41 +0300

    feat(layout-main): add block bio

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,7 @@
+// todo - заменить спец. символы юникодом, еще сделать типограф
+// todo - убрать бордеры в блоках
+// todo - пройтись по тому, что сделал и отформатировать код
+
 import "./App.css";
 import Main from "../Main/Main";
 import Header from "../Header/Header";
